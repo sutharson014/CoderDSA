96caee64029ea0cd2040e2556396e7c98a79fca9
sutharson014 <sutharson014@gmail.com>
2024-12-19 13:43:09 +0530

    Create xvalueytym.java

---
+++ xvalueytym.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Prob5 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter value x:");
+		int x = scanner.nextInt();
+		System.out.println("Enter value y:");
+		int y = scanner.nextInt();
+		for(int i=0;i<y;i++)
+		{
+			System.out.println(x);
+		}
+}
+}

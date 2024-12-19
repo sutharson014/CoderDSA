0e3f3baa989ea5bc143e5d3cb9f7b7ef631a5ebc
sutharson014 <sutharson014@gmail.com>
2024-12-19 13:36:23 +0530

    Create basic.java

---
+++ basic.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class basic {
+	public static void main(String[] args) {
+		System.out.println("Entrer your input");
+		Scanner scan=new Scanner(System.in);
+		int a=scan.nextInt();
+		char b=scan.next().charAt(0);
+		String c=scan.next();
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(c);
+	}
+
+}
